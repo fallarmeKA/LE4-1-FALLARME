82a23d773929989a184776b197eebc4667458827
KYLE FALLARME <kalfallarme@mymail.mapua.edu.ph>
2023-03-15 23:56:13 +0800

    LE4-1-FALLARME

---
+++ FallarmeLE2/FallarmeBlogDB/Stored Procedures/spPosts_Insert.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE [dbo].[spPosts_Insert]
+	@userId int,
+	@title nvarchar(150),
+	@body text,
+	@dataCreated datetime2
+
+AS
+begin
+	INSERT INTO dbo.Posts
+	(UserId, Title, Body, DataCreated)
+	VALUES
+	(@userId, @title, @body, @dataCreated)
+
+end
